ce61326c5cac6a305bfc9b1a8125657daaff3e68
alla2001 <48186577+alla2001@users.noreply.github.com>
2019-03-11 22:04:52 +0100

    add the app

---
+++ WindowsFormsApp3/WindowsFormsApp3/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace WindowsFormsApp3
+{
+    public partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.name = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ip = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Download = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Upload = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Yellow;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Modern No. 20", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(12, 28);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(126, 42);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "scan";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.Control;
+            this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dataGridView1.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.TopCenter;
+            dataGridViewCellStyle3.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle3.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle3;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.name,
+            this.ip,
+            this.Download,
+            this.Upload});
+            this.dataGridView1.EnableHeadersVisualStyles = false;
+            this.dataGridView1.GridColor = System.Drawing.Color.CadetBlue;
+            this.dataGridView1.Location = new System.Drawing.Point(3, 90);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.Size = new System.Drawing.Size(725, 279);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick_1);
+            // 
+            // name
+            // 
+            this.name.HeaderText = "Name";
+            this.name.Name = "name";
+            this.name.Width = 120;
+            // 
+            // ip
+            // 
+            this.ip.HeaderText = "ip";
+            this.ip.Name = "ip";
+            this.ip.Width = 150;
+            // 
+            // Download
+            // 
+            this.Download.HeaderText = "Download";
+            this.Download.Name = "Download";
+            // 
+            // Upload
+            // 
+            this.Upload.HeaderText = "Upload";
+            this.Upload.Name = "Upload";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.CadetBlue;
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Modern No. 20", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(144, 28);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(126, 42);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "start cut";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Tomato;
+            this.button3.FlatAppearance.BorderSize = 0;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Modern No. 20", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(276, 28);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(126, 42);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "end cut";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(728, 381);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn name;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ip;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Download;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Upload;
+    }
+}
+
---
+++ WindowsFormsApp3/WindowsFormsApp3/Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WindowsFormsApp3;
+using System.Net;
+using System.Net.NetworkInformation;
+
+namespace WindowsFormsApp3
+{
+
+    public partial class Form1 : Form
+    {
+
+
+        //System.
+        public Form1()
+        {
+            IPAddress ips=IPAddress.Parse("192.168.1.70");
+            var ping= new Ping();
+            ping.Send(ips);
+            
+           
+            Console.WriteLine(ping.Send(ips));
+            InitializeComponent();
+            addbox();
+            NetworkGateway();
+            /* List<string> devices = new List<string>();
+                  NetworkInterface[] niArr = NetworkInterface.GetAllNetworkInterfaces();
+                  Console.WriteLine("Retriving basic information of network.\n\n");
+
+                  foreach (NetworkInterface tempNetworkInterface in niArr)
+                  {
+                      devices.Add(tempNetworkInterface.Name);
+                      Console.WriteLine(tempNetworkInterface.Name);
+                  }*/
+
+        }
+
+        public void addbox()
+        {
+            var column = new DataGridViewCheckBoxColumn();
+            column.Width = 80;
+            column.HeaderText = "cut downlaod";
+            dataGridView1.Columns.Add(column);
+            var column2 = new DataGridViewCheckBoxColumn();
+            column2.Width = 80;
+            column2.HeaderText = "cut upload";
+            dataGridView1.Columns.Add(column2);
+        }
+        public void box(Dv pc)
+        {
+            var index = dataGridView1.Rows.Add();
+            dataGridView1.Rows[index].Cells["Name"].Value = pc.Name;
+            dataGridView1.Rows[index].Cells["ip"].Value = pc.Address;
+        }
+        static string NetworkGateway()
+        {
+            string ip = null;
+
+            foreach (NetworkInterface f in NetworkInterface.GetAllNetworkInterfaces())
+            {
+                if (f.OperationalStatus == OperationalStatus.Up)
+                {
+                    foreach (GatewayIPAddressInformation d in f.GetIPProperties().GatewayAddresses)
+                    {
+                        ip = d.Address.ToString();
+                        Console.WriteLine(d.Address.ToString());
+                    }
+                }
+            }
+
+            return ip;
+        }
+        public class Dv
+        {
+            public IPAddress Address { get; private set; }
+            public string Name { get; private set; }
+            public Dv(string _address, string _name)
+            {
+                Name = _name;
+
+                try
+                {
+                    Address = System.Net.IPAddress.Parse(_address);
+                }
+                catch
+                {
+                    Console.WriteLine("ip address is null");
+                }
+            }
+            public static Dv addip(string _address, string _name)
+            {
+                var pc = new Dv(_address, _name);
+                return pc;
+            }
+
+
+
+        }
+        /*public List<string> pingip(IPAddress ip)
+        {
+            List<string> _ip;
+            
+
+            return ip;
+        }*/
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            var pc = Dv.addip("192.168.1.1", "oppof9");
+            box(pc);
+            Console.WriteLine(pc.Address);
+
+
+
+
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void dataGridView1_CellContentClick_1(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
